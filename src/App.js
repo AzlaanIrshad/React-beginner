b47fdec930e867f0ef149e2ae53caa1d0b8ac43f
Azlaan Irshad <Azlaanirshad@gmail.com>
2023-01-13 15:18:19 +0100

    modified:   src/App.js 	modified:   src/components/Greet.js 	new file:   src/components/Hello.js 	new file:   src/components/Message.js 	new file:   src/components/Welcome.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+import Hello from './components/Hello'
+import Message from './components/Message'
+
+
 function App() {
   return (
     <div className="App">
-       <Greet />
+      <Message />
+       {/* <Greet name="Bruce" heroName="Batman">
+        <p>This is childeren props</p>
+        </Greet>
+       <Greet name="Clark" heroName="Superman">
+        <button>Action</button>
+       </Greet>
+       <Greet name="Diana" heroName="Woder Woman"></Greet>
+       <Welcome name="Bruce" heroName="Batman" />
+       <Welcome name="Clark" heroName="Superman" />
+       <Welcome name="Diana" heroName="Woder Woman" />
+        */}
     </div>
   );
 }
---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hello = () => {
+    // return (
+    //     <div>
+    //         <h1>Hello</h1>
+    //     </div>
+    // )
+
+    return React.createElement('div', {id: 'hello', class: 'DummyClass'}, React.createElement('h1', null, 'Hello'))
+}
+
+export default Hello
